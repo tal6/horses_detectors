d86a34b78c585e0c2f2d0487c3b599ecb146eba1
tal6 <50244401+tal6@users.noreply.github.com>
2020-07-26 23:59:25 +0300

    Update my_Object_Detection.py

--- Module_2_Object_Detection/my_Object_Detection.py
+++ Module_2_Object_Detection/my_Object_Detection.py
@@ -5,6 +5,7 @@ Created on Tue May 19 19:10:14 2020
 @author: Tal
 """
 
+# Importing the libraries
 import torch
 from torch.autograd import Variable
 import cv2
@@ -12,6 +13,7 @@ from data import BaseTransform, VOC_CLASSES as labelmap
 from ssd import build_ssd
 import imageio
 
+# Defining a function that will do the detections
 def detect(frame, net, transform):
     height, width = frame.shape[:2]
     frame_t = transform(frame)[0]
@@ -19,32 +21,38 @@ def detect(frame, net, transform):
     x = Variable(x.unsqueeze(0))
     y = net(x)
     detections = y.data
-    scale = torch.Tensor([width, height, width, height])
-    # detections = [batch, number_of_classes, number_of_occurence, (score, x0, y0, x1, y1)]
+    scale = torch.Tensor([width, heigth, width, height])
+    # detections => [batch, number_of_classes, number_of_occurence, (score, x0, y0, x1, y1)]
     for i in range(detections.size(1)):
         j = 0
         while detections[0, i, j, 0] >= 0.6:
             pt = (detections[0, i, j, 1:] * scale).numpy()
-            cv2.rectangle(frame, (int(pt[0]), int(pt[1])), (int(pt[2]), int(pt[3])), (255, 0, 0), 2)
-            cv2.putText(frame, labelmap[i-1], (int(pt[0]), int(pt[1])), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
+            cv2.rectangle(frame, (int(pt[0]), int(pt[1])), (int(pt[2]), int(pt[3])), (255, 0, 0), thickness=2)
+            cv2.putText(frame, labelmap[i - 1], (int(pt[0]), int(pt[1])), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
             j += 1
     return frame
 
-# Create the SSD neural network
+# Creating the SSD neural network
+# The model is pre-trained so we just need to apply 
+# him on our test video
 net = build_ssd('test')
 net.load_state_dict(torch.load('ssd300_mAP_77.43_v2.pth', map_location = lambda storage, loc: storage))
 
-# Create the transformation
+# Creating the transformation
 transform = BaseTransform(net.size, (104/256.0, 117/256.0, 123/256.0))
 
-# Doing some object detection on a video
+# Doing some Object Detection on a video
 reader = imageio.get_reader('funny_dog.mp4')
-fps = reader.get_meta_data()['fps'] 
-writer = imageio.get_writer('output.mp4', fps = fps)
+frames_per_sec = reader.get_meta_data()['fps']
+writer = imageio.get_writer('output.mp4', fps = frames_per_sec)
+
+# Build the new video with the detections
 for i, frame in enumerate(reader):
-    frame = detect(frame, net.eval(), transform)
-    writer.append_data(frame)
+    new_frame = detect(frame, net.eval(), transform)
+    writer.append_data(new_frame)
     print(i)
+    
 writer.close()
+
             
     
